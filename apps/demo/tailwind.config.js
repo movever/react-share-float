b633a6e2d6f8bf59f31da30e9c7b5b03008445f7
yangruijun <759089219@qq.com>
2025-09-03 23:54:14 +0800

    fix

--- apps/demo/tailwind.config.js
+++ apps/demo/tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "../../packages/react-share-float/src/**/*.{js,jsx}",
+    "../../packages/utils/src/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
--- packages/react-share-float/src/index.jsx
+++ packages/react-share-float/src/index.jsx
@@ -1,5 +1,6 @@
 "use client"
 import {useState, useEffect} from 'react';
+import './styles.css';
 import {
     FacebookShareButton,
     TwitterShareButton,
